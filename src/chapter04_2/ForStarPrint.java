d3895bdc693b54c3ce55cfb7dc4d2e78d96188d5
Kukyeon <rnrdus2000@naver.com>
2025-07-18 17:37:27 +0900

    별찍기 추가

--- src/chapter04_2/ForStarPrint.java
+++ src/chapter04_2/ForStarPrint.java
@@ -24,6 +24,15 @@ public class ForStarPrint {
 			System.out.print("*");
 		}System.out.println();
 	}
+	for (int i = 1; i <= 5; i++) {
+        for (int space = 1; space <= 5 - i; space++) {
+            System.out.print(" ");
+        }
+        for (int star = 1; star <= (2 * i - 1); star++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 		
 }
 
